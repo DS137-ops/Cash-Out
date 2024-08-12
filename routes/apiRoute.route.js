5dadef558e5588b3d25ae8472145fa9e811adf37
fayad <feadkaffoura@gmail.com>
2024-08-12 14:07:09 +0300

    new

--- routes/apiRoute.route.js
+++ routes/apiRoute.route.js
@@ -183,7 +183,7 @@ router.post('/AddReminder/:id',body,(req,res)=>{
 router.post('/addNetBill/:id',body,(req,res)=>{
   billmodel.addnetnewbillForApi(req.body.name,req.body.value,req.body.date,req.body.imgUri,req.params.id).then((rr)=>{
     authmodel.updatePadget(req.params.id,req.body.value).then(()=>{
-      authmodel.getuserPadget(req.params.id).then(()=>{
+      authmodel.getuserPadget(req.params.id).then((usrbud)=>{
         if(usrbud.padget<=0){
           sendEmail(usrbud.email,usrbud.padget)
       }
